dc619b2fb1f90b9b88c0f6dc84492c4b497b1bed
Johnathan Leech <johnathanmichael09@gmail.com>
2019-04-04 00:26:52 -0400

    completed

--- LabNumber3/Program.cs
+++ LabNumber3/Program.cs
@@ -8,9 +8,58 @@ namespace LabNumber3
 {
     class Program
     {
+        static int Number { get; set; }
+
         static void Main(string[] args)
         {
+            User.GetUserName();
+            GetNumber();
+            Console.WriteLine(GetValueFromInt());
+            Console.WriteLine($"Well, I hope that was as enjoyable for you {User.Name} as it was for me.");
+            Console.WriteLine("See ya later!");
+            Console.ReadLine();
+        }
 
+        static void GetNumber(string validity = "number")
+        {
+            try
+            {
+                Console.Write($"{User.Name} please enter a {validity} between 1 and 100: ");
+                Number = Convert.ToInt32(Console.ReadLine());
+                if (Number < 2 || Number > 99)
+                {
+                    GetNumber("VALID number");
+                }
+            }
+            catch (FormatException)
+            {
+                GetNumber("number value that is");
+            }
+        }
+
+        static string GetValueFromInt()
+        {
+            if (Number % 2 == 1 )
+            {
+                if (Number > 60)
+                {
+                    return $"Odd and greater than 60";
+                }
+
+                return $"{Number}, Odd";
+            }
+
+            if (2 <= Number && Number <= 25)
+            {
+                return "Even and less than 25";
+            }
+
+            if (26 <= Number && Number <= 60)
+            {
+                return "Even";
+            }
+
+            return $"{Number}, Even";
         }
     }
 }
---
+++ LabNumber3/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber3
+{
+    class User
+    {
+        public static string Name { get; set; }
+
+        public static void GetUserName()
+        {
+            Console.Write("Hello there. Today I will be taking a number from you and returning you a result based upon that answer. \n" +
+                                "How rude of me! I forgot to ask what your name was... ");
+            Name = Console.ReadLine();
+            Console.WriteLine($"Well it's very nice to meet you {Name}!");
+        }
+    }
+}
