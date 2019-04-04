9ff68c66f107040358e16444ccae329c64122765
Johnathan Leech <johnathanmichael09@gmail.com>
2019-04-03 22:56:49 -0400

    labnumber2 complete

---
+++ LabNumber2/LabNumber2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                GetsPerimeter();
+            }
+            while (Continue());
+
+            Console.WriteLine("Come back soon! :) ");
+            Console.ReadLine();
+        }
+
+        private static void GetsPerimeter()
+        {
+            var room = new RoomSize();
+
+            Console.Write("What is the length of the room? - ");
+            room.Length = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is the width of the room? - ");
+            room.Width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Perimeter: {room.Perimeter}, Area: {room.Area} \n");
+        }
+
+        private static bool Continue()
+        {
+            Console.Write("Would you like to calculate another room? - ");
+            string cont = Console.ReadLine().ToLower();
+            Console.WriteLine("");
+
+            if(cont == "yes")
+            {
+                return true;
+            }
+            else if(cont == "no")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input. Please respond 'Yes' or 'No'.");
+                return Continue();
+            }
+        }
+    }
+}
---
+++ LabNumber2/LabNumber2/RoomSize.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber2
+{
+    class RoomSize
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public int Area
+        {
+            get { return Length * Width; }
+        }
+
+        public int Perimeter
+        {
+            get { return Length * 2 + Width * 2; }
+        }
+    }
+}
